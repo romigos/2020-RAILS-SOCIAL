2ddc858fa4959d20b3676ccf2a637c2651f235e2
romigos <bolist@mail.ru>
2020-09-22 08:11:49 +0300

    add bootstrap

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+const webpack = require('webpack')
+environment.plugins.append("Provide", new webpack, ProvidePlugin({
+    $: 'jquery',
+    jquery: 'jquery',
+    Proper: ['proper.js', 'default']
+}))
+
 module.exports = environment.toWebpackConfig()
