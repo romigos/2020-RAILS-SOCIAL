373679899815638488747eebbcc21b437df397c7
romigos <bolistovskiyroman@gmail.com>
2020-10-01 12:22:14 +0300

    add some ection and fix some bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,18 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def topic
+    @topic = Topic.find_by(alias: params[:topic])
+    @posts = @topic.posts
+    render 'index'
+  end
+
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @topics = Topic.all()
     @post = Post.find(params[:id])
   end
 
@@ -12,13 +23,22 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body, :title_id))
+    @post = Post.new(post_params)
     @post.save
 
     redirect_to '/posts/' + @post.id.to_s
   end
 
-  def edit
+  def update
     @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to '/posts/' + @post.id.to_s
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :topic_id)
   end
 end
+
